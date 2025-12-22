087d365ce8e6e1cb8a1c9dbf6d514b274b2b055f
AyushNaikReal <ayushnaik2506@gmail.com>
2025-12-22 10:40:43 +0530

    added space

--- main_code.py
+++ main_code.py
@@ -1,6 +1,7 @@
 to_do_list = ['hi','hi'] # This list will store to-do of user
 while True:
      # code for options
+  print( )
   print("""Press : 
   1 for View all To-Dos
   2 for Insert To-Dos
@@ -20,8 +21,10 @@ while True:
    entry=input("Enter your to-do (one to at a time): ")
    to_do_list.append(entry)
    print(entry, " - is added in your to-do list")
+   print( )
    print("'Do you want to see all entries in to-do list?'")
    nk=input("If yes press 'y' or no press 'n': ")
    if nk=='y':
     for index, item in enumerate(to_do_list, start=1):
      print(index,item)
+
